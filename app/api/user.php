f9dd59dc17807292041c09d29676f88d2c33c9ac
倒霉狐狸(公司PC) <admin@evildao.com>
2019-01-31 14:02:29 +0800

    feat: 添加登陆接口，并补上注册接口的sessionKey生成

--- app/api/user.php
+++ app/api/user.php
@@ -1,6 +1,6 @@
 <?php
 /**
- *
+ * 用户信息处理接口模块
  * Created by PhpStorm.
  * User: 倒霉狐狸
  * Date: 2018-11-27
@@ -9,8 +9,10 @@
 
 namespace app\api;
 
+use app\Service\Utils;
 use Respect\Validation\Validator as v;
 use zhimiao\Data;
+use zhimiao\Request;
 
 class user
 {
@@ -21,11 +23,51 @@ class user
         $this->db = Data::pdo();
     }
 
+    /**
+     * 登陆
+     * @param string $phone
+     * @param string $password
+     * @return array
+     */
     public function signin($phone = null, $password = null)
     {
-
+        if (!v::phone()->validate($phone)) {
+            return [-5, null, '手机号无法识别'];
+        }
+        if (!v::length(6, 32)->validate($password)) {
+            return [-4, null, '密码长度不合法(6位-32位)'];
+        }
+        $lock_key = [
+            'signin:phone:'. $phone,
+            'signin:ip:'. md5(Request::getIp())
+        ];
+        if (!Utils::cacheNumLock($lock_key[0]) || !Utils::cacheNumLock($lock_key[1], 15)) {
+            return [-3, null, '尝试次数过频，请稍后重试'];
+        }
+        $user_info = $this->db->quickPrepare('select `uid`,`phone`,`password`,`status` from member where phone=:phone', [':phone' => $phone])->getOnce();
+        if ($user_info === false) {
+            return [-5, null, '用户不存在'];
+        }
+        if ($user_info['status'] != 1) {
+            return [-5, null, '用户状态异常'];
+        }
+        if (!password_verify($password, $user_info['password'])) {
+            return [-5, null, '密码输入错误'];
+        }
+        return [1, [
+            'user_info' => $user_info,
+            'sessionKey' => Utils::createSessionKey($user_info['uid'])
+        ]];
     }
 
+    /**
+     * 注册账号
+     * @param string $phone
+     * @param string $password
+     * @param string $code
+     * @param string $token
+     * @return array|int
+     */
     public function signup($phone = null, $password = null, $code = null, $token = null)
     {
         $VerifyService = new \app\Service\Verify();
@@ -46,8 +88,11 @@ class user
             return [-6, null, '注册失败，系统繁忙或者此手机号已经注册了'];
         }
         $uid = $this->db->lastInsertId();
-
-
+        if ($uid > 0) {
+            $sessionKey = Utils::createSessionKey($uid);
+            return [1, ['sessionKey' => $sessionKey]];
+        }
+        return 0;
     }
 
 }
