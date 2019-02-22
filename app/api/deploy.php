be296f37d10240cb5ceacc2865f8cb84f4ec1218
倒霉狐狸(公司PC) <admin@evildao.com>
2019-02-22 19:31:51 +0800

    feat: 添加首页列表接口

--- app/api/deploy.php
+++ app/api/deploy.php
@@ -9,6 +9,9 @@
 
 namespace app\api;
 
+use Respect\Validation\Validator as v;
+use zhimiao\Data;
+use zhimiao\Request;
 
 class deploy
 {
@@ -19,8 +22,47 @@ class deploy
         $this->uid = \app\Service\Verify::isLogin();
     }
 
-    public function index()
+    /**
+     * 首页列表
+     * @param int $pageid 分页id，lastid
+     * @param null $search 搜索关键词
+     * @return array
+     */
+    public function index($pageid = 0, $search = null)
     {
-        
+        $sql = 'select * from deploy where uid=:uid';
+        $params = [
+            ':uid' => $this->uid
+        ];
+        if (!empty($search)) {
+            $sql .= ' and title like :search';
+            $params[':search'] = "%{$search}%";
+        }
+        if ($pageid > 0) {
+            $sql .= ' and id<:pageid';
+            $params[':pageid'] = $pageid;
+        }
+        $sql .= ' order by id desc limit 20';
+        $data = Data::pdo()->quickPrepare($sql, $params)->toArray();
+        return [1, ['data' => $data]];
+    }
+
+    /**
+     * 设置部署数据
+     */
+    public function set()
+    {
+        $id = Request::post('id', 0);
+        $title = Request::post('title');
+        $deploy_type = Request::post('deploy_type');
+        $remote_url = Request::post('remote_url', '');
+        $local_path = Request::post('local_path', '');
+        $branch = Request::post('branch', '');
+        $before_command = Request::post('before_command', '');
+        $after_command = Request::post('after_command', '');
+        if (!v::notEmpty()->validate($title)) {
+            return [-4, null, '标题不能为空'];
+        }
+
     }
 }
