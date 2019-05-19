1c2549c74995e4f2eadd923e402e1e28d12b0f99
小子(LT) <admin@evildao.com>
2019-05-19 22:20:27 +0800

    feat: 安装模块新增配置文件创建测试

--- install/install.go
+++ install/install.go
@@ -2,15 +2,41 @@ package install
 
 import (
 	"fmt"
+	"github.com/astaxie/beego/config"
+	"github.com/satori/go.uuid"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"time"
 )
 
 func Install() bool {
+	cfg()
+	//fmt.Println(os.Hostname())
+	//fmt.Println(common.GetLocalMac())
 	//绑定用户
 	if !binUser() {
 		return false
 	}
-
 	return true
+}
+
+func cfg() {
+	_, err := os.Stat("config.ini")
+	if err != nil {
+		file, err := os.Create("config.ini")
+		if err != nil {
+			panic("文件无法写入")
+		}
+		file.Close()
+	}
+	cfg, err := config.NewConfig("ini", "config.ini")
+	if err != nil {
+		panic("配置文件读取错误")
+	}
+	cfg.Set("zhimiao::device", uuid.NewV4().String())
+	cfg.SaveConfigFile("config.ini")
+
 }
 
 func binUser() bool {
@@ -27,4 +53,24 @@ func binUser() bool {
 	}
 	fmt.Println("数据读取成功：" + UID)
 	return true
+}
+
+func RequestJob(UID string, HostName string) {
+	client := &http.Client{
+		Transport: &http.Transport{
+			ResponseHeaderTimeout: time.Second * 3,
+		},
+	}
+	url := "http://localhost:1305/index.php?c=api&a=regServer&uid=" + UID + "&hostname=" + HostName
+	resp, err := client.Get(url)
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+	body, _ := ioutil.ReadAll(resp.Body)
+	resp.Body.Close()
+	if string(body) != "2" {
+		return
+	}
+
 }
