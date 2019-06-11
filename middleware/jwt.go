80a381b423185ae36e3c6029df1ba400f675b108
倒霉狐狸(公司PC-01) <admin@evildao.com>
2019-06-11 19:10:55 +0800

    jwt拦截新增UID信息设置传递

--- middleware/jwt.go
+++ middleware/jwt.go
@@ -1,27 +1,29 @@
 package middleware
 
 import (
-	"github.com/dgrijalva/jwt-go"
 	"github.com/gin-gonic/gin"
 	"net/http"
+	"strconv"
 	"tools-server/common/utils"
 )
 
 // JWT is jwt middleware
 func JWT() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		var msg string
+		var msg string = "登陆信息获取失败"
 		token := c.GetHeader("LOGIN-KEY")
-		if token == "" {
-			msg = "登录信息获取失败"
-		} else {
-			_, err := utils.ParseToken(token)
+		if token != "" {
+			str, err := utils.ParseToken(token)
 			if err != nil {
-				switch err.(*jwt.ValidationError).Errors {
-				case jwt.ValidationErrorExpired:
-					msg = "登录过期"
-				default:
-					msg = "登录验证失败"
+				print(err.Error())
+				msg = "登录验证失败"
+			}
+			if str != "" {
+				c.Set("LOGIN-TOKEN", str)
+				uid, _ := strconv.Atoi(str)
+				c.Set("UID", uid)
+				if uid > 0 {
+					msg = ""
 				}
 			}
 		}
@@ -31,7 +33,6 @@ func JWT() gin.HandlerFunc {
 				"code": -1,
 				"msg":  msg,
 			})
-
 			c.Abort()
 			return
 		}
