0185e71ed7bbda2c40e0f393e79e7c901eda5078
倒霉狐狸(波波网吧) <mail@xiaoliu.org>
2019-07-21 15:29:17 +0800

    调试部署命令执行，关闭panic错误抛出

--- qiansi-client/deploy/deploy.go
+++ qiansi-client/deploy/deploy.go
@@ -13,9 +13,18 @@ func Run() {
 	TaskList := []models.Deploy{}
 	_ = request.GetDeployTask(&TaskList)
 	for _, v := range TaskList {
+		// 执行前置命令
+		if v.BeforeCommand != "" {
+			RunShell(v.LocalPath, v.BeforeCommand)
+		}
+		// 选择部署操作
 		switch v.DeployType {
 		case 1:
 			Git(&v)
+		}
+		// 执行后置命令
+		if v.AfterCommand != "" {
+			RunShell(v.LocalPath, v.AfterCommand)
 		}
 	}
 }
@@ -30,13 +39,11 @@ func LogPush(format string, v ...interface{}) {
 }
 
 func RunShell(work_path, cmd string) error {
+	defer shell.ErrExit()
 	if runtime.GOOS == "windows" {
-		shell.Shell = []string{""}
+		shell.Shell = []string{"C:\\Windows\\System32\\cmd.exe", "/c"}
 	}
-	// shell.Shell = []string{}
-	// shell.Trace = true
 	shell.Panic = false
-
 	cmd = strings.ReplaceAll(cmd, "\r\n", "\n")
 	cmds := strings.Split(cmd, "\n")
 	for _, v := range cmds {
@@ -46,8 +53,8 @@ func RunShell(work_path, cmd string) error {
 		res := shell.Cmd(v)
 		res.SetWorkDir(work_path)
 		o := res.Run()
+		o.Wait()
 		LogPush("正在执行命令: %s, \n结果输出:\n %s\n错误输出:\n %s", v, o.String(), o.Error().Error())
 	}
-
 	return nil
 }
--- qiansi-client/deploy/deploy_test.go
+++ qiansi-client/deploy/deploy_test.go
@@ -3,10 +3,12 @@ package deploy
 import "testing"
 
 func TestRunShell(t *testing.T) {
-	RunShell("C:\\", `
-dir
-
-tasklist
+	RunShell("cs:\\", `
+piqng baidu.com
 
 `)
+	RunShell("c:\\", `
+pinqg baidu.com
+`)
+
 }
